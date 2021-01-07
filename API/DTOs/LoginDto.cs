a1f2d6206a565fee1cfa34f9f37265c1bd7eb921
James Rodwell-Shield <jamesrs222@gmail.com>
2021-01-07 21:53:45 +0000

    End of Section 11

--- API/DTOs/LoginDto.cs
+++ API/DTOs/LoginDto.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
+
 namespace API.DTOs
 {
     public class LoginDto
     {
         public string Username { get; set; }
         public string Password { get; set; }
+
     }
 }
